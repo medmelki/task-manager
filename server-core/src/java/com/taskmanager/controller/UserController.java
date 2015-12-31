53b6a5bc08aeb8146269db00c04f47108459b222
mmelki <mmelki@kapit.fr>
2015-12-31 17:32:02 +0100

    Set Maven Modules architecture

--- server-core/src/java/com/taskmanager/controller/UserController.java
+++ server-core/src/java/com/taskmanager/controller/UserController.java
@@ -3,6 +3,7 @@ package com.taskmanager.controller;
 import com.taskmanager.model.User;
 import com.taskmanager.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,24 +17,28 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @RequestMapping(method = RequestMethod.GET)
     public String listAll(Model model) {
         model.addAttribute("users", userService.findAll());
         return "user";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SUPERADMIN')")
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String addUser(@ModelAttribute User user) {
         userService.create(user);
         return "redirect:/user";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SUPERADMIN')" )
     @RequestMapping(value = "update", method = RequestMethod.POST)
     public String updateUser(@ModelAttribute User user) {
         userService.update(user);
         return "redirect:/user";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SUPERADMIN')")
     @RequestMapping(value = "remove", method = RequestMethod.POST)
     public String deleteUser(@ModelAttribute User user) {
         userService.delete(user);
